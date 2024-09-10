48b3cd9ad058151f0961d238a240032d6c9c3032
Reshmafareed <163650782+Reshmafareed@users.noreply.github.com>
2024-09-10 22:20:02 +0500

    Add files via upload

---
+++ Stripping_Names.ts
@@ -0,0 +1,7 @@
+const PersonName: string = '    \n\tReshma Fareed\t\n    ';
+
+ console.log(PersonName);
+
+ const without_whitespace: string = PersonName.trim();
+
+ console.log(without_whitespace);
